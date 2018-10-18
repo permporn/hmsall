59d1321527b24ffaf0f7ae2ab6cf1f686225ec8e
Permporn <permporn.k@gmail.com>
2018-10-18 09:40:51 +0700

    update cofig

--- cardpro/report/config/config_multidb.php
+++ cardpro/report/config/config_multidb.php
@@ -10,15 +10,15 @@ $dbname = 'ajtongmath_scho';
 
 //** SERVER **//
 
-// $dbuser = "ajtongmath_scho" ; 
+$dbuser = "ajtongmath_scho" ; 
 
-// $dbpass= "72o35XDJ";
+$dbpass= "72o35XDJ";
 
 //** LOCAL **//
 
-$dbuser = "root" ; 
+// $dbuser = "root" ; 
 
-$dbpass= "1234";
+// $dbpass= "1234";
 
 $con_ajtongmath_scho = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
 
@@ -44,15 +44,15 @@ $dbname = 'ajtongmath_self';
 
 //** SERVER **//
 
-// $dbuser = "ajtongmath_self" ; 
+$dbuser = "ajtongmath_self" ; 
 
-// $dbpass= "5TEwC8yip";
+$dbpass= "5TEwC8yip";
 
 //** LOCAL **//
 
-$dbuser = "root" ; 
+// $dbuser = "root" ; 
 
-$dbpass= "1234";
+// $dbpass= "1234";
 
 $con_ajtongmath_self = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
 
