9cafdb2580db4e11ec7e4d7c4f7e37e42c60da15
Permporn <permporn.k@gmail.com>
2019-06-18 15:45:07 +0700

    update add event

--- cardpro/even.php
+++ cardpro/even.php
@@ -10,17 +10,28 @@ include("ck_session.php");
 
 ?>
 
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
 
+<link href="http://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+
 <?php  include("script_link.php");?>
 
 </head>
+
+<script src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+
+<script>
+
+$(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+
+</script>
+
 
 <body>
 
@@ -28,25 +39,18 @@ include("ck_session.php");
 
 <script language="JavaScript">
 
- 
-
 function ChkSubmit(result){
 
 	if(document.getElementById("even").value == "")
 
 		{
-
 			alert('กรุณาใส่ข่าวสารที่ต้องการแจ้ง');
 
 			return false;
-
 		}
-
 }
 
 </script>
-
-
 
 <div id="container">
 
@@ -56,7 +60,7 @@ function ChkSubmit(result){
 
   <h1>แจ้งข่าวสาร</h1>
 
-  <form action="saveeven.php" name="frmMain" method="post" onSubmit="return ChkSubmit();">
+  <form action="saveeven.php?type=add" name="frmMain" method="post" onSubmit="return ChkSubmit();">
 
   <table class="tbl-border" cellpadding="0" cellspacing="1" width="90%" align="center">
 
@@ -107,66 +111,29 @@ function ChkSubmit(result){
 	INNER JOIN account a 
 
 	ON e.idstaff = a.accid";
-
+	
 	$objQuery = mysqli_query($con_ajtongmath_scho, $strSQL) or die ("Error Query [".$strSQL."]");
 
 	$Num_Rows = mysqli_num_rows($objQuery);
 
-	$Per_Page = 15;   // Per Page
-
-	$Page = $_GET["Page"];
-
-	if(!$_GET["Page"])
-
-	{
-		$Page=1;
-	}
-
-	$Prev_Page = $Page-1;
-
-	$Next_Page = $Page+1;
-
-	$Page_Start = (($Per_Page*$Page)-$Per_Page);
-
-	if($Num_Rows<=$Per_Page)
-	{
-		$Num_Pages =1;
-	}
-	else if(($Num_Rows % $Per_Page)==0)
-	{
-		$Num_Pages =($Num_Rows/$Per_Page) ;
-	}
-	else
-	{
-		$Num_Pages =($Num_Rows/$Per_Page)+1;
-
-		$Num_Pages = (int)$Num_Pages;
-	}
-
-	$strSQL .=" order  by evenid DESC LIMIT $Page_Start , $Per_Page";
-
 	$objQuery = mysqli_query($con_ajtongmath_scho, $strSQL) or die ("Error Query [".$strSQL."]");
 
 ?>
-<table class="tbl-border" cellpadding="0" cellspacing="1" width="90%" align="center">
-
+<table id="myTable" class="display" style="width:100%">
+	<thead>
 	  <tr>
-
-		<td width="" class="tbl2" style="white-space: nowrap;" align="center"> วันที่</th>
-
-		<td width="" class="tbl2" style="white-space: nowrap;" align="center"> ข่าว</th>
-
-        <td width="" class="tbl2" style="white-space: nowrap;" align="center"> ผู้แจ้ง</th>
-
-        <?php if($objResultSTT["status"]=="Manager"||$objResultSTT["status"]=="ADMIN" || $objResultSTT["status"]=="admin") {?>
-
-        <td width="" class="tbl2" style="white-space: nowrap;" align="center"> ลบ</th>
-
-        <?php }?>
-
+			<td width="" class="tbl2" style="white-space: nowrap;" align="center"> วันที่</th>
+			<td width="" class="tbl2" style="white-space: nowrap;" align="center"> ข่าว</th>
+			<td width="" class="tbl2" style="white-space: nowrap;" align="center"> ผู้แจ้ง</th>
+			<?php if($objResultSTT["status"]=="Manager"||$objResultSTT["status"]=="ADMIN" || $objResultSTT["status"]=="admin") {?>
+			<td width="" class="tbl2" style="white-space: nowrap;" align="center"> ลบ</th>
+			<?php }?>
 	  </tr>
-
+	</thead>	
+	<tbody>
 	<?php
+
+	$num = 0;
 
 	while($objResult = mysqli_fetch_array($objQuery))
 	{
@@ -176,61 +143,21 @@ function ChkSubmit(result){
 
 		else{$tblyy = "tblyy";}
 	?>
-	  <tr>
-
-		<td width="" class="<?=$tblyy?>" style="white-space: nowrap;" align="center"><?=DateThai($objResult["date"]);?></td>
-
-        <td width="" class="<?=$tblyy?>" style="white-space: nowrap;" align="center"><?=$objResult["even"];?></td>
-
-        <td width="" class="<?=$tblyy?>" style="white-space: nowrap;" align="center">
-
-        <?=$objResult["name"];?>
-
-        </td>
-
-        <?php if($objResultSTT["status"]=="Manager"||$objResultSTT["status"]=="ADMIN" || $objResultSTT["status"]=="admin") {?>
-
-        <td width="" class="<?=$tblyy?>" style="white-space: nowrap;" align="center">
-
-        <a href="JavaScript:if(confirm('Confirm Delete?')==true){window.location='saveeven.php?evenid=<?=$objResult["evenid"];?>&type=del';}"><img src="images/delete-2.png"></a>
-
-        </td>
-
-        <?php } ?>
-
-	  </tr>
-    <?php $i++; }?>
-
-    </table>
-
-    <div align="right"><p>
-
-    Total <?= $Num_Rows;?> Record : <?=$Num_Pages;?> Page :
-
-	<?php
-
-	if($Prev_Page)
-	{
-		echo " <a href='$_SERVER[SCRIPT_NAME]?Page=$Prev_Page&txtKeyword=$_GET[h_arti_id]'><< Back</a> ";
-	}
-	for($i=1; $i<=$Num_Pages; $i++){
-
-		if($i != $Page){
-			echo "[ <a href='$_SERVER[SCRIPT_NAME]?Page=$i&txtKeyword=$_GET[h_arti_id]'>$i</a> ]";
-		}
-		else{
-			echo "<b> $i </b>";
-		}
-	}
-	if($Page!=$Num_Pages)
-	{
-		echo " <a href ='$_SERVER[SCRIPT_NAME]?Page=$Next_Page&txtKeyword=$_GET[h_arti_id]'>Next>></a> ";
-	}
-	?>
-    </p></div>
-
-  </div>
-
+	<tr>
+		<td  style="white-space: nowrap;" align="center"><?=DateThai($objResult["date"]);?></td>
+			<td  style="white-space: nowrap;" align="center"><?=$objResult["even"];?></td>
+			<td  style="white-space: nowrap;" align="center">
+			<?=$objResult["name"];?>
+			</td>
+			<?php if($objResultSTT["status"]=="Manager"||$objResultSTT["status"]=="ADMIN" || $objResultSTT["status"]=="admin") {?>
+			<td  style="white-space: nowrap;" align="center">
+			<a href="JavaScript:if(confirm('Confirm Delete?')==true){window.location='saveeven.php?evenid=<?=$objResult["evenid"];?>&type=del';}"><img src="images/delete-2.png"></a>
+			</td>
+			<?php } ?>
+	</tr>
+  <?php $i++; }?>
+	</tbody>
+</table>
 </html>
 
 <?php mysqli_close($con_ajtongmath_scho);?>
--- cardpro/saveeven.php
+++ cardpro/saveeven.php
@@ -1,29 +1,49 @@
-<?
-session_start();
-include("config.inc.php");
-include("funtion.php");
-include("ck_session.php");
-
-	$s=date('Y-m-d');
-	$type = $_GET["type"];
-	$idstaff = $_POST["idstaff"];
-if($type == "del"){
-	$strSQL = "DELETE FROM even ";
-	$strSQL .="WHERE evenid = '".$_GET["evenid"]."' ";
-	$objQuery = mysql_query($strSQL);
-	if(!$objQuery){
-		echo "Error del [".mysql_error()."]";
-	}
-}else{
-	$strSQL = "INSERT INTO even ";
-	$strSQL .="(even,date,idstaff) ";
-	$strSQL .="VALUES ";
-	$strSQL .="('".$_POST["even"]."','".$s."','".$idstaff."') ";
-	$objQuery = mysql_query($strSQL);
-	if(!$objQuery)
-	{
-		echo "Error Save [".mysql_error()."]";
-	}
-}
-header("location:even.php");
+<?php
+session_start();
+
+include("config.inc.php");
+
+	$s=date('Y-m-d');
+
+	$type = $_GET["type"];
+
+	$idstaff = $_POST["idstaff"];
+
+if($type == "del"){
+
+	$strSQL = "DELETE FROM even ";
+
+	$strSQL .="WHERE evenid = '".$_GET["evenid"]."' ";
+
+	$objQuery = mysqli_query($con_ajtongmath_scho, $strSQL);
+
+	if(!$objQuery){
+		echo "Error del [".mysqli_error()."]";
+	}else {
+		header("location:even.php");
+	}
+
+}else{
+
+	$even = $_POST["even"];
+
+	$strSQL = "INSERT INTO even ";
+
+	$strSQL .="(even,date,idstaff) ";
+
+	$strSQL .="VALUES ";
+
+	$strSQL .="('".$even."','".$s."','".$idstaff."') ";
+
+	$objQuery = mysqli_query($con_ajtongmath_scho, $strSQL);
+
+	if(!$objQuery){
+		echo "Error Save [".mysqli_error()."]";
+	}else {
+		header("location:even.php");
+	}
+}
+
+
+
 ?>
